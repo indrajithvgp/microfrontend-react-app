03660554fc9c4e233acda3c0acea7ed303911ffc
Indrajith V <indrajithvgp@yahoo.com>
2021-07-01 14:57:23 +0530

    code tweak -02

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -2,7 +2,7 @@ const {merge} = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const commonConfig = require('./webpack.common')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const packageJson = require('../package.json')
+const packageJSON = require('../package.json')
 
 const domain = process.env.PRODUCTION_DOMAIN
 
@@ -17,7 +17,7 @@ const prodConfig = {
             remotes: {
                 marketing: `marketing@${domain}/marketing/remoteEntry.js`
             },
-            shared: packageJson.dependencies
+            shared: packageJSON.dependencies
         }),
         
     ]
