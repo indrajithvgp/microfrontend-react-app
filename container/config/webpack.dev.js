12f2d59f729e218ee0bfb0150f1631ce0d30081e
Indrajith V <indrajithvgp@yahoo.com>
2021-07-01 14:09:39 +0530

    config files edited

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const {merge} = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const commonConfig = require('./webpack.common')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
 
 
 const devConfig = {
@@ -17,7 +17,8 @@ const devConfig = {
             name:"container",
             remotes:{
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-            }
+            },
+            shared: packageJson.dependencies
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import {mount} from 'marketing/MarketingApp'
-
-
-console.log(mount)
+import MarketingApp from './components/MarketingApp'
 
 export default() => {
     return (
         <div>
             Hi there..!
+            <hr/>
+            <MarketingApp/>
         </div>
     )
 }
